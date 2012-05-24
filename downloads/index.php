9cd9a18f3108fca619aed0fddae2843972b47450
lgoubet <lgoubet>
2012-05-24 09:37:02 +0000

    yet another test

--- downloads/index.php
+++ downloads/index.php
@@ -1,20 +1,4 @@
-<?php  																														
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
-$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		
-include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-	
-	$pageTitle 		= "EMF Compare - Download";
-	
-	# Paste your HTML content between the EOHTML markers!
-	$html = <<<EOHTML
-	<div id="midcolumn">
-	EMF Compare Downloads
-	</div>
-	EOHTML;
-
-	# Generate the web page
-	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
-	#echo $headlines;
+<?php
+	header("Location: /emf/compare/download.php");
+	exit;
 ?>
