c559376b55e1417574e0147ab09b38b0559a5d24
lgoubet <lgoubet>
2012-05-24 12:39:47 +0000

    test

--- downloads/index.php
+++ downloads/index.php
@@ -9,7 +9,7 @@
 	$PWD = $App->getDownloadBasePath() . "/modeling/emf/compare/downloads/drops";
 
 	$html  = '<div id="midcolumn">';
-	$html .= $_SERVER["DOCUMENT_ROOT"];
+	$html .= $PWD;
 	$html .= "</div>";
 	
 	# Generate the web page
