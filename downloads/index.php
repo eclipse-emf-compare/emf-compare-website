2196de3809e4e71bc44935d203eb8f1106419f24
lgoubet <lgoubet>
2012-05-24 11:47:52 +0000

    Testing variable value.

--- downloads/index.php
+++ downloads/index.php
@@ -1,8 +1,12 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+	require_once ("../../includes/buildServer-common.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/downloads-scripts.php");
+	
 	$pageTitle 		= "EMF Compare - Download";
 
 	$html  = '<div id="midcolumn">';
 	$html .= "EMFCompare downloads";
+	$html .= $projct;
 	$html .= "</div>";
 	
 	# Generate the web page
