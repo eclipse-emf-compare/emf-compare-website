88ab8077497465cd93d99755a11d9714abaf8b2e
lgoubet <lgoubet>
2012-05-24 14:28:46 +0000

    test

--- downloads/custom-scripts.php
+++ downloads/custom-scripts.php
@@ -31,7 +31,7 @@
 			foreach ($types as $type => $names) {
 				if ($type == "R" && isset($oldBuilds[$version][$branch][$type])) {
 					$id = $oldBuilds[$version][$branch][$type][0];
-					$releases[$version][substr($id, 1) . $type] = $branch . $type;
+					$releases[$version][$branch][substr($id, 1) . $type] = $branch . $type;
 				} else if (array_key_exists($version, $oldBuilds) && array_key_exists($branch, $oldBuilds[$version]) && array_key_exists($type, $oldBuilds[$version][$branch]) && is_array($oldBuilds[$version][$branch][$type])) {
 					$newBuilds[$version][$branch][$type] = $oldBuilds[$version][$branch][$type];
 					rsort($newBuilds[$version][$branch][$type]);
@@ -41,5 +41,35 @@
 		return array($newBuilds,$releases);
 	}
 	
-	
+	function generateHTMLReleaseList($releases) {
+		$html = "";
+		if (sizeof($releases) > 0) {
+			$html .= "<div class=\"homeitem3col\">\n";
+			$html .= "<h3>Releases</h3>\n";
+			$html .= "<ul>\n";
+			
+			foreach ($releases as $version => $branchReleases) {
+				$html .= "<li>";
+				$html .= "$version Releases\n";
+				$html .= "<ul>\n";
+				
+				foreach ($branchReleases as $rID => $rbranch) {
+					$branch = preg_replace("/.$/", "", $rbranch);
+					$ID = preg_replace("/^(\d{12})([IMNRS])$/", "$2$1", $rID);
+					
+					$html .= "<li>";
+					$html .= "$branch\n";
+					
+					
+					$html .= "<\li>\n";
+				}
+				
+				$html .= "<\ul>\n";
+				$html .= "<\li>\n";
+			}
+				
+			$html .= "</ul>\n";
+			$html .= "</div>\n";
+		}
+	}
 ?>
